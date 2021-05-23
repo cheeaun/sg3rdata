0ea5271f044984e2df0d953f49d30275e085b59f
Lim Chee Aun <cheeaun@gmail.com>
2021-05-24 00:52:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Singapore 3R data
+===
+
+Data from https://www.nea.gov.sg/our-services/waste-management/3r-programmes-and-resources/recycling-collection-points
---
+++ postprocess.ts
@@ -0,0 +1,7 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts';
+
+const filename = Deno.args[0];
+const data = await readJSON(filename);
+
+const newFilename = filename.replace(/\.txt$/, '.json');
+await writeJSON(newFilename, JSON.parse(data).SrchResults);
