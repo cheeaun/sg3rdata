3af92aa9f7843d60eb925115a494671ef90e0b34
Lim Chee Aun <cheeaun@gmail.com>
2021-05-24 07:57:07 +0800

    Generate GeoJSON too

--- postprocess.ts
+++ postprocess.ts
@@ -3,5 +3,21 @@ import { readJSON, writeJSON } from 'https://deno.land/x/flat/mod.ts';
 const filename = Deno.args[0];
 const data = await readJSON(filename);
 
-const newFilename = filename.replace(/\.txt$/, '.json');
-await writeJSON(newFilename, JSON.parse(data).SrchResults.slice(1));
+const jsonFilename = filename.replace(/\.txt$/, '.json');
+const jsonData = JSON.parse(data).SrchResults.slice(1);
+
+const geojsonFilename = filename.replace(/\.txt$/, '.geojson');
+const geoJSONData = {
+  type: 'FeatureCollection',
+  features: jsonData.map(d => ({
+    type: 'Feature',
+    properties: d,
+    geometry: {
+      type: 'Point',
+      coordinates: d.LatLng.split(',').reverse(),
+    },
+  })),
+};
+
+await writeJSON(jsonFilename, jsonData);
+await writeJSON(geojsonFilename, geoJSONData);
