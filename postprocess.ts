77ef333e180df3334ce25b752c3ae693adedf206
Lim Chee Aun <cheeaun@gmail.com>
2021-05-24 01:03:35 +0800

    Remove first row

--- postprocess.ts
+++ postprocess.ts
@@ -4,4 +4,4 @@ const filename = Deno.args[0];
 const data = await readJSON(filename);
 
 const newFilename = filename.replace(/\.txt$/, '.json');
-await writeJSON(newFilename, JSON.parse(data).SrchResults);
+await writeJSON(newFilename, JSON.parse(data).SrchResults.slice(1));
